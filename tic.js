1d3dc6fc403597938b58b785fca6346400e22f92
128bit <128bit@eclipso.eu>
2023-09-21 19:54:31 +0300

    Add methods for playField

---
+++ tic.js
@@ -0,0 +1,42 @@
+"use strict"
+const playField = (() => {
+    const field = new Array(9).fill("");
+    const clearField = () => {
+        field.fill("");
+    }
+    const checkWin = () => {
+        for (let i = 0; i < 3; i++) {
+            const result = field[i] + field[i+3] + field[i+6];
+            if (result === "XXX" || result === "OOO") {
+                endGame(result[0]);
+            }
+        }
+        const leftDiag = field[0] + field[4] + field[8];
+        const rightDiag = field[2] + field[4] + field[6];
+        if (leftDiag === "XXX" || leftDiag === "OOO") {
+            endGame(leftDiag[0]);
+            return;
+        }
+        else if (rightDiag === "XXX" || rightDiag === "OOO") {
+            endGame(rightDiag[0]);
+            return;
+        }
+        else if (field.indexOf("") === -1) {
+            endGame("");
+        }
+    }
+    const endGame = (result) => {
+        if (result === "") {
+            console.log("It's a draw");
+        }
+        else {
+            console.log(`${result} is the winner`);
+        }
+    }
+    const makeMove = (player, position) => {
+        if (field[position] === "") {
+            field[position] = player;
+            checkWin();
+        }
+    }
+})();
